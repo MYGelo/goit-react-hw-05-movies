d166b18fdff9a279ef5517472192049a3dbb63e6
MYGelo <112314820+MYGelo@users.noreply.github.com>
2023-04-04 10:06:21 +0000

    Deploying to gh-pages from @ MYGelo/goit-react-hw-05-movies@71b9f70fa6c93f999af80614c9280b3c0e8cdddd 🚀

---
+++ static/js/862.94fff7c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[862],{862:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u="NotFound_container__1fusd",o=t(184);function c(){return(0,o.jsx)("div",{className:u,children:"NotFound"})}}}]);
+//# sourceMappingURL=862.94fff7c9.chunk.js.map
