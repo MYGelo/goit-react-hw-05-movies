bb1f72a0249a7dc9d2c293f580100bcad7f630a8
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-04-04 16:49:40 +0300

    fix credits

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -2,8 +2,6 @@ import { searchCredits } from 'API';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const tmdb = 'https://image.tmdb.org/t/p/w500';
-
 export default function Credits() {
   const [credits, setCredits] = useState([]);
   const { movieId } = useParams();
@@ -11,11 +9,7 @@ export default function Credits() {
   useEffect(() => {
     searchCredits(movieId).then(credit => setCredits(credit));
   }, [movieId]);
-
-  const q = credits.map(item => item.img);
-  const w = q ? `hello` : `hello2`;
-
-  console.log(w);
+  const tmdb = 'https://image.tmdb.org/t/p/w500';
 
   return (
     <div>
@@ -23,9 +17,11 @@ export default function Credits() {
         {credits.map(item => (
           <li key={item.id}>
             <img
-              // {item.img ? (dsdf) : (dsa)}
-              src={`${tmdb}${item.img}`}
-              // src={'https://via.placeholder.com/150x100'}
+              src={
+                item.img
+                  ? `${tmdb}${item.img}`
+                  : `https://via.placeholder.com/150x100`
+              }
               alt=""
               height={'150px'}
               width={'100px'}
